73ec6ef6f981e8f76da55c4eab98f2cd2d84c752
jamerson-mt <jjamersonmt@gmail.com>
2026-02-25 08:55:44 -0300

    feat: 04-input-user.cs, entrada de dados de usuario

---
+++ 01-fundamentals-of-csharp/04-input-user.cs
@@ -0,0 +1,9 @@
+
+//variables
+string name;
+
+//insertion of data
+Console.WriteLine("insert your name:");
+name = Console.ReadLine(); 
+Console.WriteLine($"Hello, {name}! Welcome to my Sistem!");
+
