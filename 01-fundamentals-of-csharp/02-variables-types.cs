f7c7a418eab951c03bc96e87b9effd23e9800ab4
José Jamerson <jjamersonmt@hotmail.com>
2026-03-12 19:34:07 -0300

    chore: ajuste na declaracao de VAR, var nao pode ser declarada sem inicializao da mesma

--- 01-fundamentals-of-csharp/02-variables-types.cs
+++ 01-fundamentals-of-csharp/02-variables-types.cs
@@ -3,7 +3,17 @@ int x = 10; //valores inteiros
 string name = "Jamerson"; //recebe valores de caracteres
 double price = 29.90; //Recebe valores decimais;
 bool stats = true; //recebe valores logicos, verdadeiro ou falso
-var others; //neste caso ele pode receber qualquer tipo de valor, mas seu uso nao é recomendado na escrita de codigo
+var message = "Uma Variavel implicita deve ser sempre inicializada, se nao ocorrera em erro"; //neste caso ele pode receber qualquer tipo de valor, mas seu uso nao é recomendado na escrita de codigo
 
 
 // Estes são tipos simples de variaveis existentes, mas existem outros tipo mais complexos e especificos dependedo do constexto de uso.
+
+// exiba todas variaveis 
+Console.WriteLine(x);
+Console.WriteLine(name);
+Console.WriteLine(price);
+Console.WriteLine(stats);
+Console.WriteLine(message);
+
+
+
