1900227f740340d702bfd9c1cadf8b5afa985c1d
José Jamerson <jjamersonmt@hotmail.com>
2026-03-13 12:53:41 -0300

    feat: full

---
+++ 01-fundamentals-of-csharp/02.1-variables-types.cs
@@ -0,0 +1,6 @@
+
+string name = "bob";
+int number = 3;
+double numberFloat = 34.4;
+
+Console.WriteLine($"Hello {name}, you have {number} messages in your box. the temperature is {numberFloat} celsius");
