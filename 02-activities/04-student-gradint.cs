cfdfc88108a8493e4538f9754bdbe1bd8bc8e873
José Jamerson <jjamersonmt@hotmail.com>
2026-03-13 21:26:58 -0300

    feat: Configutarion of insertion students and points in loop structure, and print values

--- 02-activities/04-student-gradint.cs
+++ 02-activities/04-student-gradint.cs
@@ -1,21 +1,40 @@
 //VARIABLES
 var students = new List<string>();
 var points = new List<double>();
-string value = "";
-// inicialization students name and points
-Console.WriteLine("Insira o nome do aluno:");
-value = Console.ReadLine() ?? ""; // null-coalescing operator to handle null input
-students.Add(value);
-Console.WriteLine($"Insira a nota de {students[0]}");
-if(double.TryParse(Console.ReadLine(), out double nota)){
-    points.Add(nota);
-}else{
-    nota = 0;
-    Console.WriteLine("Nota inserida invalida! Atribuiremos a nota 0 ao aluno");
-    points.Add(nota);
-}
+string name;
 
-Console.WriteLine(
-$@"Aluno: {students[0]}
-nota: {points[0]}"
-);
+// inicialization students name and points
+Console.WriteLine("Quantos alunos?");
+if (int.TryParse(Console.ReadLine(), out int q))
+{
+    while (q > 0)
+    {
+        //INSERTION STUDENT
+        Console.WriteLine("Insira o nome do aluno:");
+        name = Console.ReadLine() ?? ""; // null-coalescing operator to handle null input
+        students.Add(name);
+        //INSERTION STUDENT POINT
+        for (int qNota = 0; qNota < 5; qNota++)
+        {
+            // INSERCAO DAS 5 NOTAS DO ESTUDANTE
+            Console.WriteLine($"Insira a nota {qNota+1} de {students[q - q]}");
+            if (double.TryParse(Console.ReadLine(), out double nota))
+            {
+                points.Add(nota);
+            }
+            else
+            {
+                // CASO ERRO NA INSERCAO DA NOTA SERA ATRIBUIDO O VALOR 0
+                nota = 0;
+                Console.WriteLine("Nota inserida invalida! Atribuiremos a nota 0 ao aluno");
+                points.Add(nota);
+            }
+        }
+        Console.WriteLine($"Aluno: {students[q - q]}");
+        foreach (var p in points)
+        {
+            Console.WriteLine($"Nota: {p}");
+        }
+        q--; //decremento a quantidade para quando inserir todos studantes encerrar
+    }
+}
