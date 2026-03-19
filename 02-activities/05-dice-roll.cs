97bf75acfab3adc393f4eba1d1ce6ad9b6c98586
jamerson-mt <jjamersonmt@gmail.com>
2026-03-19 12:17:12 -0300

    feat: insercao da logica de lancamento de dado, onde gera um valor aleatorio para cada jogador e imprime no console

--- 02-activities/05-dice-roll.cs
+++ 02-activities/05-dice-roll.cs
@@ -1,4 +1,5 @@
 //lANCAMENTO DE DADO POR JOGADORES PARA OBTER O MAIOR NUMERO
+Random dice = new();
 
 Console.WriteLine("Informe quantos jogadores irao jogar:");
 string value = Console.ReadLine() ?? "0";
@@ -11,3 +12,10 @@ else
     Console.WriteLine($"Quantidade de jogadores nao informada: {qJogadores}");
 }
 
+List<int> dicePoints = new(); //lista de pontos do dados
+for (int i = 0; i < qJogadores; i++)
+{
+    int roll = dice.Next(1, 6);
+    dicePoints.Add(roll);
+    Console.WriteLine($"Jogador {i+1}, lancou o dado: {dicePoints[i]}");
+}
